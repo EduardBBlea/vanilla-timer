01ca63d3232e6fe6040ce9874e9b262afd00fb33
Eduard B <edd.bba@gmail.com>
2022-09-16 20:29:22 +0100

    created the html & css

---
+++ app.js
@@ -0,0 +1,4 @@
+const watch = document.querySelector(".display__watch-numbers");
+const startButton = document.querySelector("#start");
+const stopButton = document.querySelector("#stop");
+const resetButton = document.querySelector("#reset");
